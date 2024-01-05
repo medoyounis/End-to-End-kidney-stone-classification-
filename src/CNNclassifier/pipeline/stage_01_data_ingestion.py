2246d3f76eaa8366c9c576f71f47d7195e31875d
medoyounis <Mohammedyounis1996@gmail.com>
2024-01-05 17:04:30 -0600

    Update stage_01_data_ingestion.py

--- src/CNNclassifier/pipeline/stage_01_data_ingestion.py
+++ src/CNNclassifier/pipeline/stage_01_data_ingestion.py
@@ -1,6 +1,6 @@
-from src.cnnClassifier.config.configuration import ConfigurationManager
-from src.cnnClassifier.components.data_ingestion import DataIngestion
-from src.cnnClassifier import logger
+from src.CNNclassifier.config.configuration import ConfigurationManager
+from src.CNNclassifier.components.data_ingestion import DataIngestion
+from src.CNNclassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
