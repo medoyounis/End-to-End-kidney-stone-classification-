5358527453f149318d6b98f407089e7d4f81fc14
medoyounis <Mohammedyounis1996@gmail.com>
2024-01-05 17:04:00 -0600

    Update prepare_base_model.py

--- src/CNNclassifier/components/prepare_base_model.py
+++ src/CNNclassifier/components/prepare_base_model.py
@@ -3,7 +3,7 @@ import urllib.request as request
 from zipfile import ZipFile
 import tensorflow as tf
 from pathlib import *
-from src.cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+from src.CNNclassifier.entity.config_entity import PrepareBaseModelConfig
 
 
 class PrepareBaseModel:
